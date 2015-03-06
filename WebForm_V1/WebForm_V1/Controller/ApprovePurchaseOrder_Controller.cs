5cc062b5e6a0d9fe1b4b79f724b80d9a1587e93e
sneha2606 <sneha2606@gmail.com>
2015-03-06 13:34:47 +0800

    Controllers

---
+++ WebForm_V1/WebForm_V1/Controller/ApprovePurchaseOrder_Controller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LU
+{
+    public class ApprovePurchaseOrder_Controller
+    {
+    }
+}
---
+++ WebForm_V1/WebForm_V1/Controller/ApproveStockAdjustment_Controller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LU
+{
+    public class ApproveStockAdjustment_Controller
+    {
+    }
+}
